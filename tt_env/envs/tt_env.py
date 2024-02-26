83fdbd1e75c5ddaf707f1e7603171af1bcfed4a9
Adam <adamhung@umich.edu>
2024-02-26 11:36:07 +1100

    wfe

--- tt_env/envs/tt_env.py
+++ tt_env/envs/tt_env.py
@@ -7,20 +7,31 @@ import pkg_resources
 import cv2
 import time
 import random
+import math
 
 class TTEnv(gym.Env):
     metadata = {"render_modes": ["human", "rgb_array"]}
 
-    def __init__(self, render_mode=None):
+    def __init__(self, render_mode=None, y = 0):
+        self.render_mode = render_mode
         self.robot_path = pkg_resources.resource_filename(__name__, 'robot/robot.urdf')
         self.table_path = pkg_resources.resource_filename(__name__, 'table/robot.urdf')
         self.ball_path = pkg_resources.resource_filename(__name__, 'ball/robot.urdf')
-        self.max_steps = 250
+
+        self.max_steps = 500
         self.steps_taken = 0
-        self.joints = [0, 1, 2, 3]
+
+        self.joints = [0, 1, 2, 3, 4]
         self.numJoints = len(self.joints)
-        self.success = False
-        self.render_mode = render_mode
+        
+        self.y = y
+
+        # monitor vars
+        self.episode_count = 0
+        self.ball_in_count = 0
+        self.ball_touch_count = 0
+        self.d2t_sum = 0
+
         if self.render_mode == "human":
             p.connect(p.GUI)
         else:
@@ -30,115 +41,135 @@ class TTEnv(gym.Env):
         p.setRealTimeSimulation(0)
         p.resetSimulation()
 
-        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(35,), dtype=float)
-        self.action_space = spaces.Box(low=-1, high=1, shape=(4,), dtype=float)
-
-    def get_success(self):
-        return self.success
+        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(64,), dtype=float)
+        self.action_space = spaces.Box(low=-1, high=1, shape=(self.numJoints,), dtype=float)
 
     def get_obs(self):
-
-        link_positions = []
-        link_orientations = []
-        joint_positions = []
-        joint_velocities = []
-        for link in range(self.numJoints-1):
-            link_state = p.getLinkState(self.robot, link)
-            link_positions += link_state[0]
-            link_orientations += link_state[1]
-        for joint in self.joints:
-            joint_state = p.getJointState(self.robot, joint)
-            joint_positions.append(joint_state[0])
-            joint_velocities.append(joint_state[1])
-        
+        joint_positions = [p.getJointState(self.robot, joint)[0] for joint in self.joints]
         ball_position = list(p.getBasePositionAndOrientation(self.ball)[0])
-        ball_velocity = list(p.getBaseVelocity(self.ball)[0])
+        current_obs = np.array(joint_positions+ball_position)
 
-        return np.array(link_positions+link_orientations+joint_positions+joint_velocities+ball_position+ball_velocity)
-    
-    def is_dead(self):
-        return False
+        self.obs = self.obs[1:]
+        self.obs = np.vstack((self.obs, current_obs))
+
+        return self.obs.flatten()
     
     def reset(self,seed=None, options = None): 
         super().reset(seed=seed)
+        self.terminated = False
+        self.truncated = False
         self.steps_taken = 0
+        self.state = 0
+        self.obs = np.zeros((8, 8))
+
+        self.episode_count += 1
+
         p.resetSimulation()
         p.setGravity(0,0,-9.8)
         self.plane = p.loadURDF("plane.urdf") 
-        self.robot = p.loadURDF(self.robot_path, [0,0,0],useFixedBase = 1)
-        self.table = p.loadURDF(self.table_path, [0.374,0,0],useFixedBase = 1)
-        self.ball = p.loadURDF(self.ball_path, [0.7,0,0.1],useFixedBase = 0)
+        self.robot = p.loadURDF(self.robot_path, [-1.5,0,0], useFixedBase = 1)
+        self.table = p.loadURDF(self.table_path, [0,0,0],useFixedBase = 1)
+
+        start, v = self.get_trajectory()
+        self.ball = p.loadURDF(self.ball_path, start)
+        p.resetBaseVelocity(self.ball, linearVelocity = v)
+        #self.ball = p.loadURDF(self.ball_path, [0.3,0,0.1],useFixedBase = 0)
+        #p.resetBaseVelocity(self.ball, linearVelocity = [-1.3,0.2,1.4])
+
         p.changeDynamics(self.ball, -1, restitution = 0.9)
         p.changeDynamics(self.table, -1, restitution = 0.9)
         p.changeDynamics(self.robot, 2, restitution = 0.9)
-        #p.resetBaseVelocity(self.ball, linearVelocity = [random.uniform(-2.1, -1.3),random.uniform(-0.5, 0.5),1.2])
-        p.resetBaseVelocity(self.ball, linearVelocity = [-1.5,0.3,1.2])
-
-        self.state = 0
-        self.terminated = False
-        self.truncated = False
 
         observation = self.get_obs()
         info = {"info":"hi"}
 
         return observation, info
     
-    def getReward(self):
-        if p.getContactPoints(self.ball, self.plane):
-                self.terminated = True
-                return 0
-        elif self.state == 0: # ball bounces on table
+    def get_trajectory(self):
+        #goal = [random.uniform(-1.37, 0), random.uniform(-0.7625, 0.7625), 0.1]
+        #start = [random.uniform(0, 2.0), random.uniform(-0.7625, 0.7625), random.uniform(0.1, 1)]
+        y = random.uniform(-self.y, self.y)
+        start = [1.5, 0, 0.5]
+        goal = [-1.0, y, 0.1]
+        
+        #vz = random.uniform(3, 10)
+        vz = 3
+        t = (vz + math.sqrt(vz**2 + 19.6*(start[2]+goal[2])))/9.8
+        vx = (goal[0] - start[0])/t
+        vy = (goal[1] - start[1])/t
+        v = [vx,vy,vz]
+        return start, v
+
+    def get_reward(self):
+
+        if self.state == 0: # ball bounces on table
             if p.getContactPoints(self.table, self.ball):
                 self.state = 1
-                return 0
-            elif p.getContactPoints(self.robot, self.ball):
-                self.terminated = True
-                return -0.5 # robot hits ball before bounce
-            else:
-                return 0
         elif self.state == 1: # robot hits ball
             if p.getContactPoints(self.robot, self.ball, 3):
                 self.state = 2
+                self.ball_touch_count += 1
                 return 1 # robot hits ball correctly
             elif p.getContactPoints(self.ball):
                 self.terminated = True
-                return -0.5 # ball hits something other than paddle
-            else:
-                return 0
-        elif self.state == 2: # ball lands somewhere
-            point =  p.getContactPoints(self.table, self.ball)
-            if point and point[0][5][0] > 0.374:
+        elif self.state == 2: # ball lands
+            point = p.getContactPoints(self.ball, self.plane) or p.getContactPoints(self.ball, self.table)
+            if point:
+                point = p.getContactPoints(self.ball)
+                d2t = self.d2t(point[0][5][0],point[0][5][1])
+                self.d2t_sum += d2t
                 self.terminated = True
-                self.success = True
-                return 10 # ball goes in
-            elif p.getContactPoints(self.ball):
-                self.terminated = True
-                return -0.5 # something else happens
-            else:
-                return 0
+                if d2t == 0:
+                    self.ball_in_count += 1
+                return 1/(d2t+0.01)
+        return 0
+    
+    def d2t(self,x,y): # distance to table
+        if x < 0:
+            dx = -x
+        elif x > 1.37:
+            dx = x - 1.37
+        else:
+            dx = 0
+        
+        if y > 0.7625:
+            dy = y - 0.7625
+        elif y < -0.7625:
+            dy = 0.7625 - y
+        else:
+            dy = 0
+            
+        return math.sqrt(dx**2 + dy**2)
         
     def step(self, action):
+        if self.episode_count >= 1000:
+            print("balls in / 1000 = ", self.ball_in_count)
+            print("balls touched / 1000 = ", self.ball_touch_count)
+            print("average d2t for touched balls = ", self.d2t_sum/(self.ball_touch_count+0.0001))
+            self.episode_count = 0
+            self.ball_in_count = 0
+            self.ball_touch_count = 0
+            self.d2t_sum = 0
         
-        action = action*5
-        p.setJointMotorControlArray(self.robot,self.joints, controlMode=p.VELOCITY_CONTROL, forces = [0]*self.numJoints)
+        actions = action*20
+        
         p.setJointMotorControlArray(
             bodyUniqueId = self.robot, 
             jointIndices = self.joints, 
-            controlMode = p.TORQUE_CONTROL, 
-            forces = list(action))
+            controlMode = p.VELOCITY_CONTROL, 
+            targetVelocities = actions,
+            forces = [500]*self.numJoints)
         
         p.stepSimulation()
         self.steps_taken += 1
         if self.steps_taken >= self.max_steps:
             self.truncated = True
 
-        dead = self.is_dead()
-        if dead:
-            self.terminated = True
-
         observation = self.get_obs()
 
-        reward = self.getReward()
+        ball_reward = self.get_reward()
+        #control_cost = np.sum(np.square(actions))
+        reward = ball_reward#-control_cost
 
         if self.render_mode == "human":
             self.render_frame()
@@ -152,16 +183,16 @@ class TTEnv(gym.Env):
             return self.render_frame()
 
     def render_frame(self):
-        focus_position,_ = p.getBasePositionAndOrientation(self.robot)
+        """focus_position,_ = p.getBasePositionAndOrientation(self.robot)
         focus_position = tuple([focus_position[0] + 0.2,focus_position[1] + 0.2,focus_position[2]])
         p.resetDebugVisualizerCamera(
-            cameraDistance=0.8, 
-            cameraYaw = 60, 
-            cameraPitch = -20, 
+            cameraDistance=0.9, 
+            cameraYaw = 40, 
+            cameraPitch = -12, 
             cameraTargetPosition = focus_position
-        )
+        )"""
         if self.render_mode == "human":
-            time.sleep(0.0005)
+            time.sleep(0.000)
         if self.render_mode == "rgb_array":
             h,w = 4000, 4000
             image = np.array(p.getCameraImage(h, w)[2]).reshape(h,w,4)
